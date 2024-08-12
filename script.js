0294f1a98f33e977f95fb40cdc317470bc6522e9
Hawa Diallo <hawa.diallo26@compscihigh.org>
2024-08-12 00:43:26 +0000

    back tics

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let userInput = prompt("Where are the horses?");
 //Create a short story (at least 2-3 sentences) using these variables. The story should make sense even if it's silly or whimsical. 
 //Concatenate your variables into a single string that forms a complete sentence or sentences. 
 
-let story = 'Once upon a time a girl, her father and thier ${word} entered a new village. They had ${number} ${word}. ${userInput}, asked the father. Days later they found out it was ${isHungry} that ${calculatedNumber} of them died due to the new air in the village.';
+let story = `Once upon a time a girl, her father and thier ${word} entered a new village. They had ${number} ${word}. ${userInput}, asked the father. Days later they found out it was ${isHungry} that ${calculatedNumber} of them died due to the new air in the village.`;
 
 //Use console.log() to output the concatenated string with your story to the console.
 
